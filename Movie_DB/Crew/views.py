7d65327c5dcc72b00f5ff275b4f15c0048a81054
Hari <harilalsunil2@gmail.com>
2023-09-07 13:18:03 +0530

    Initial commit

---
+++ Movie_DB/Crew/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from Crew.models import *
+from Movies.models import *
+# Create your views here.
+
+def view_actor(request,aslug):
+    context = Actor.objects.get(slug=aslug)
+    filmography=Movie.objects.filter(actors__name=context.name)
+    return render(request,'crew_detail.html',{'context':context,'filmography':filmography,
+                                              'count':filmography.count()})
+def view_director(request,dslug):
+    context=Director.objects.get(slug=dslug)
+    filmography=Movie.objects.filter(director__name=context.name)
+    return render(request,'crew_detail.html',{'context':context,'filmography':filmography,
+                                              'count':filmography.count()})
+def view_writer(request,wslug):
+    context=Writer.objects.get(slug=wslug)
+    filmography = Movie.objects.filter(writers__name=context.name)
+    return render(request, 'crew_detail.html',{'context': context, 'filmography': filmography,
+                                               'count': filmography.count()})
+def view_cinematographer(request,cslug):
+    context=Cinematographer.objects.get(slug=cslug)
+    filmography = Movie.objects.filter(cinematographer__name=context.name)
+    return render(request, 'crew_detail.html',{'context': context, 'filmography': filmography,
+                                               'count': filmography.count()})
+
+
